bb4aeb940db8f7d6d25c1d82910a03c39c3006b7
Mate Pušić <orakmoyaamitsira@gmail.com>
2024-05-23 16:38:03 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+import './bootstrap';
+import { createInertiaApp } from '@inertiajs/svelte'
+import Layout from "./Layout.svelte";
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
+        let page = pages[`./Pages/${name}.svelte`];
+        return { default: page.default, layout: page.layout || Layout };
+    },
+    setup({ el, App, props }) {
+        new App({ target: el, props })
+    },
+})
